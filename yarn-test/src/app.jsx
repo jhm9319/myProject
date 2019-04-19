79e098ffdd829fe66fbbc0dd3f608ff7b64bdaad
jhm9319 <jiaheming0421@qq.com>
2019-04-19 11:28:34 +0800

    使用yarn,webpack搭建react项目框架

---
+++ yarn-test/src/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'font-awesome/css/font-awesome.min.css';
+import './index.scss';
+import './index.css';
+
+ReactDOM.render(
+    <div>
+        Hello World!!
+        <i className="fa fa-bandcamp"></i>
+    </div>,
+    document.getElementById('root')
+);
